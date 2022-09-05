b61edbfb03059aad90b1c16540f6c50439516212
Ali Arain <deistali2@gmail.com>
2022-09-05 07:08:56 +0500

    CompletedJob Model

--- backend/job/models.py
+++ backend/job/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidators, MaxValueValidators
+from django.contrib.gis.db import models as gismodels
+from django.contrib.gis.geos import PointField
+from datetime import *
+from django.contrib.auth.models import User
 
 
 class JobType(models.TextChoices):
@@ -28,6 +33,13 @@ class Experiance(models.TextChoices):
     THREE_YEAR_PLUS=' 3 Years and above'
 
 
+# Making Last Date To Apply To Minimum 10 Days
+def return_date_time():
+    now = datetime.now()
+    return now + timedelta(days=10)
+
+
+
 class Job(models.Model):
     title = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True)
@@ -44,12 +56,19 @@ class Job(models.Model):
         default=Education.Bachelors
     )
     industry= models.CharField(
-        max_length=10,
+        max_length=30,
         choices=Industry.choices,
         default=Industry.IT
     )
     experiance = models.CharField(
-        max_length=10,
+        max_length=20,
         choices=Experiance.choices,
         default=Experiance.NO_EXPERIANCE
     )
+    salary = models.IntegerField(default=1, validators=[MinValueValidators(1), MaxValueValidators(1000000)])
+    positions = models.IntegerField(default=1)
+    company = models.CharField(max_length=100, null=True)
+    point = gismodels.PointField(default=Point(0.0,0.0))
+    lastDate = models.DateTimeField(default=return_date_time)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
